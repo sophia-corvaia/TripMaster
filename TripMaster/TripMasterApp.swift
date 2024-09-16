4bc0d26b400979ab60f12ecdb5e4ba48ec16ea46
Sophia Corvaia <37689558+sophia-corvaia@users.noreply.github.com>
2024-09-16 18:34:06 +0200

    Initial Commit

---
+++ TripMaster/TripMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripMasterApp.swift
+//  TripMaster
+//
+//  Created by Sophia on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TripMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
